084102cf072ba0b5029766ff97a1a14f8c974ac6
WaAaaAterfall <eleanor1025pan@outlook.com>
2023-02-27 17:54:54 -0500

    two ways of daemon

--- main.cpp
+++ main.cpp
@@ -1,9 +1,28 @@
 #include <cstdlib>
-
+#include <sys/types.h>
+#include <unistd.h>
 #include "proxy.hpp"
+
 int main() {
   //become a daemon
-  //be_daemon()
+  pid_t pid = getpid();
+  if ((pid = fork()) < 0){
+    std::cerr << "fork fail";
+    exit(1);
+  }
+  else if (pid > 0){ /* parent */
+    exit(0);
+  }
+  /* proxy child A */
+  setsid();
+  if ((pid = fork()) < 0){
+      std::cerr << "fork fail";
+      exit(1);
+  }
+  else if (pid != 0){  
+      exit(0);
+  }
+  
   int listener = proxy::proxy_init_listener();
   proxy::cache = new Cache(30);
 
